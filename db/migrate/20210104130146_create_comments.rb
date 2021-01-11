8d22e68b8d94b6356a78a2f46860fcdbba2f9e2c
TOSHI-hiro <inchs528@gmail.com>
2021-01-11 22:42:41 +0900

    comment_db_commit

--- db/migrate/20210104130146_create_comments.rb
+++ db/migrate/20210104130146_create_comments.rb
@@ -1,5 +1,5 @@
 class CreateComments < ActiveRecord::Migration[6.0]
-  def changep
+  def change
     create_table :comments do |t|
       t.text :text, null: false
       t.references :user, foreign_key: true
